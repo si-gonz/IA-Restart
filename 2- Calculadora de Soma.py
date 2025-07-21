cc07c6db3078d45c7f0bd3f86c8e448e08211b44
si.gonz <you@example.com>
2025-07-21 20:48:25 -0300

    Primeiro commit

---
+++ 2- Calculadora de Soma.py
@@ -0,0 +1,11 @@
+"""2- Calculadora de Soma
+* Desenvolva um programa que soma dois números.
+* Use as variáveis numero1 = 12 e numero2 = 14.
+* O programa deve calcular a soma e exibir o resultado."""
+
+numero1= 12
+numero2=14
+resultado = 0
+resultado =numero1+numero2
+
+print(resultado)
